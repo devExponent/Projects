36ce5bd0c895984ca616373177dd6fa77adf095b
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-09-18 09:23:46 +0300

    Bismillah TotheEnd InshaALLah

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,18 @@
 import reactImage from "./assets/react-core-concepts.png";
+import componentImg from "./assets/config.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+function CoreConcept(props) {
+  return (
+    <>
+      <li>
+        <img src={props.image} alt="" />
+        <h3>{props.title}</h3>
+        <p>{props.textDescription}</p>
+      </li>
+    </>
+  );
+}
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -29,6 +42,17 @@ function App() {
       <main>
         <h2>Time to get started!</h2>
       </main>
+      <section id="core-concepts">
+        <ul>
+          <CoreConcept
+            title="Components"
+            textDescription="The core UI building block"
+            image={componentImg}
+          />
+          <CoreConcept title="" tecxtDescription="" image={componentImg} />
+          <CoreConcept title="" tecxtDescription="" image={componentImg} />
+        </ul>
+      </section>
     </div>
   );
 }
