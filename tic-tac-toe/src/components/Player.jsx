fc62cbc84acf929d7d4e8d19760db3f1d61cc7ca
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-09-25 22:03:07 +0300

    Bismillah TotheEnd InshaALLah

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 const Player = ({ name, symbol }) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
   function handleEdit() {
     setIsEditing((editing) => !editing);
+  }
+  function handleInput(event) {
+    setPlayerName(event.target.value);
   }
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" value="fggf" required />
+          <input type="text" onChange={handleInput} required />
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
