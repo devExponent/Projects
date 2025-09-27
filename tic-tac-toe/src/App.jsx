72c2809364878fab0c0597219eddaa8ceac1e44c
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-09-28 00:56:35 +0300

    Bismillah TotheEnd InshaALLah

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,7 +1,12 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActiveplayer] = useState("X");
+  function handleSelectSquare() {
+    setActiveplayer((curActivePlayer) => (curActivePlayer === "X" ? "0" : "X"));
+  }
   return (
     <main>
       <div id="game-container">
@@ -9,7 +14,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="Y" />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} />
       </div>
       LOG
     </main>
