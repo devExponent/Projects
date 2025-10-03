3706119b5cbf8585d283e648027a0dc63525f4a5
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-03 09:54:32 +0300

    Onto the next

---
+++ investment_calculator/src/components/Inputs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Inputs = () => {
+  return (
+    <>
+      <main>
+        <input type="text" placeholder="Hello" />
+      </main>
+    </>
+  );
+};
+
+export default Inputs;
