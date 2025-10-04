e5200a41ede3fe1b972926e46bfe93a902a8fc46
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-05 00:49:48 +0300

    Bismillah TotheEnd InshaALLah

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,6 +1,18 @@
-import Inputs from "./components/Inputs";
+import { useState } from "react";
+// import Inputs from "./components/Inputs";
 function App() {
-  return <Inputs />;
+  // return <Inputs />;
+  const [data, setData] = useState("");
+  function handleChange(event) {
+    setData(event.target.value);
+  }
+
+  return (
+    <>
+      <input type="text" value={data} onChange={handleChange} />
+      <h2>{data}</h2>
+    </>
+  );
 }
 
 export default App;
