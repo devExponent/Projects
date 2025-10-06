3124c92eb77598be89e4a59eba4c2df908bcbdd6
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-06 16:18:22 +0300

    Bismillah TotheEnd InshaALLah

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 function App() {
+  const [item, setItems] = useState("");
+  const [todo, setTodo] = useState([]);
+  function handleChange(e) {
+    setItems(e.target.value);
+    console.log(item);
+  }
+
+  function Add() {
+    setTodo((prev) => [...prev, item]);
+    setItems("");
+  }
+
   return (
     <>
-      <UserInputs />
+      <div>
+        <input type="text" value={item} onChange={handleChange} />
+        <button onClick={Add}>Add to do</button>
+        <h2>
+          {todo.map((items, index) => (
+            <li key={index}>{items}</li>
+          ))}{" "}
+        </h2>
+      </div>
     </>
   );
 }
--- investment_calculator/src/components/UserInputs.jsx
+++ investment_calculator/src/components/UserInputs.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const UserInputs = ({ amount, dynamicChange }) => {
   return (
     <section>
