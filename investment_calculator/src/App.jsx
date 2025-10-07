21f729940cfdd7218ec1e1a8052633c34b33d9f9
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-07 10:11:39 +0300

    Bismillah TotheEnd InshaALLah

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,28 +1,8 @@
-import { useState } from "react";
+import UserInputs from "./components/UserInputs";
 function App() {
-  const [item, setItems] = useState("");
-  const [todo, setTodo] = useState([]);
-  function handleChange(e) {
-    setItems(e.target.value);
-    console.log(item);
-  }
-
-  function Add() {
-    setTodo((prev) => [...prev, item]);
-    setItems("");
-  }
-
   return (
     <>
-      <div>
-        <input type="text" value={item} onChange={handleChange} />
-        <button onClick={Add}>Add to do</button>
-        <h2>
-          {todo.map((items, index) => (
-            <li key={index}>{items}</li>
-          ))}{" "}
-        </h2>
-      </div>
+      <UserInputs />
     </>
   );
 }
--- investment_calculator/src/components/UserInputs.jsx
+++ investment_calculator/src/components/UserInputs.jsx
@@ -1,20 +1,23 @@
-const UserInputs = ({ amount, dynamicChange }) => {
+import { useState } from "react";
+
+const UserInputs = () => {
+  const [change, setHandleChange] = useState("");
+  const [chy, setChy] = useState("44");
+  function changing(e) {
+    setHandleChange(e.target.value);
+    setChy(e.target.value);
+  }
   return (
     <section>
       <div id="user-input">
         <div className="input-group">
           <p>
             <label>Initial Investment</label>
-            <input
-              type="number"
-              required
-              value={amount}
-              onChange={dynamicChange}
-            />
+            <input type="number" required value={change} onChange={changing} />
           </p>
           <p>
-            <label>ANnual Investment</label>
-            <input type="number" required />
+            <label>Annual Investment</label>
+            <input type="number" required value={chy} onChange={changing} />
           </p>
         </div>
         <div className="input-group">
@@ -28,6 +31,8 @@ const UserInputs = ({ amount, dynamicChange }) => {
           </p>
         </div>
       </div>
+      <p>{change}</p>
+      <p>{chy}</p>
     </section>
   );
 };
