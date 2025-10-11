008f44ff26e40342ad3be797f0fc475b6e4e1305
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-11 21:24:21 +0300

    getting it

--- finance/src/App.jsx
+++ finance/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import UserInputs from "../src/components/userInput";
 import InvestmentTable from "./components/InvestmentTable";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: "11",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: "",
+  });
+
+  const handleUserInput = (name, value) => {
+    setUserInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
-      <UserInputs />
+      <UserInputs value={userInput.initialInvestment} />
       <InvestmentTable />
+      <p>{userInput.initialInvestment}</p>
     </>
   );
 }
--- finance/src/components/userInput.jsx
+++ finance/src/components/userInput.jsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
-
-const userInput = () => {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: "",
-    annualInvestment: "",
-    expectedReturn: "",
-    duration: "",
-  });
-
-  const handleUserInput = (name, value) => {
-    setUserInput((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+const userInput = ({ onHandleInput }) => {
   return (
     <>
       <section id="user-input">
         <div className="input-group">
           <p>
             <label>Initial Investment</label>
-            <input
-              type="number"
-              value={userInput.initialInvestment}
-              onChange={(e) =>
-                handleUserInput("initialInvestment", e.target.value)
-              }
-            />
+            <input type="number" onChange={onHandleInput} />
           </p>
           <p>
             <label>Annual Investment</label>
@@ -43,7 +22,6 @@ const userInput = () => {
             <input type="number" />
           </p>
         </div>
-        <p>{userInput.initialInvestment}</p>
       </section>
     </>
   );
