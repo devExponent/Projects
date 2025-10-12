86a319d8a2cd2671c462edc219a5c48bc0557832
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-12 09:32:16 +0300

    getting it

--- finance/src/components/InvestmentTable.jsx
+++ finance/src/components/InvestmentTable.jsx
@@ -1,22 +1,28 @@
 import { calculateInvestmentResults } from "../util/investment";
+
 const InvestmentTable = ({ input }) => {
+  const result = calculateInvestmentResults(input);
+  console.log(result);
   return (
-    <table id="result">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{input.initialInvestment}</td>
+          </tr>
+        </tbody>
+      </table>
+      {result.year}
+    </>
   );
 };
 
