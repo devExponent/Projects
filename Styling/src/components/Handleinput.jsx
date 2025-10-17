3dd81b6481a81d21b5ad14aea200bfa80ada2027
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-17 06:08:16 +0300

    year

--- Styling/src/components/Handleinput.jsx
+++ Styling/src/components/Handleinput.jsx
@@ -1,33 +1,9 @@
-import React from "react";
-import { useState } from "react";
-
 const Handleinput = () => {
-  const [handleInput, setHandleInput] = useState(false);
-  const [NhandleInput, setNHandleInput] = useState(false);
-
-  function monitor() {
-    setHandleInput(true);
-  }
-  function Nmonitor() {
-    setNHandleInput(true);
-  }
-
-  const see = handleInput;
-  const Nsee = NhandleInput;
+  // return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
   return (
-    <div>
-      <h1 style={{ background: see ? "green" : Nsee ? "yellow" : "" }}>
-        CSS is great!
-      </h1>
-      <menu>
-        <li>
-          <button onClick={monitor}>Yes</button>
-        </li>
-        <li>
-          <button onClick={Nmonitor}>No</button>
-        </li>
-      </menu>
-    </div>
+    <>
+      <h1>Bag</h1>
+    </>
   );
 };
 
