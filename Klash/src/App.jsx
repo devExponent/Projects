c8754edbb19ba41ad571aafdc5810c3d920e8da1
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-17 23:29:40 +0300

    year

--- Klash/src/App.jsx
+++ Klash/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-xl md:text-9xl font-bold underline">Hello world!</h1>
       <h2>Hello</h2>
     </>
   );
