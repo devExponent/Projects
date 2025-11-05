23cc9953304284210ae5244d4cf042f16eed23c0
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-11-05 13:10:58 +0200

    yeah

--- Countdown/src/components/Player.jsx
+++ Countdown/src/components/Player.jsx
@@ -1,7 +1,7 @@
-export default function Player({ playerNames, onSetName, ref }) {
+export default function Player({ enterPlayerNames, onSetName, ref }) {
   return (
     <section id="player">
-      <h2>Welcome {playerNames ?? "Unknown Entity"}</h2>
+      <h2>Welcome {enterPlayerNames ?? "Unknown Entity"}</h2>
       <p>
         <input type="text" ref={ref} />
         <button onClick={onSetName}>Set Name</button>
